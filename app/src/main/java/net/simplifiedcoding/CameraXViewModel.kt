544b90304fcdd85ee631a066743eec0f1d0f5e07
belal-khan <belal.khan@getstream.io>
2023-04-20 19:04:57 +0530

    CameraXViewModel Added

---
+++ app/src/main/java/net/simplifiedcoding/CameraXViewModel.kt
@@ -0,0 +1,31 @@
+package net.simplifiedcoding
+
+import android.app.Application
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.util.concurrent.ExecutionException
+
+class CameraXViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val cameraProviderLiveData: MutableLiveData<ProcessCameraProvider> = MutableLiveData()
+    val processCameraProvider: LiveData<ProcessCameraProvider>
+        get() {
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(getApplication())
+            cameraProviderFuture.addListener(
+                {
+                    try {
+                        cameraProviderLiveData.setValue(cameraProviderFuture.get())
+                    } catch (e: ExecutionException) {
+                        e.printStackTrace()
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+                },
+                ContextCompat.getMainExecutor(getApplication())
+            )
+            return cameraProviderLiveData
+        }
+}
