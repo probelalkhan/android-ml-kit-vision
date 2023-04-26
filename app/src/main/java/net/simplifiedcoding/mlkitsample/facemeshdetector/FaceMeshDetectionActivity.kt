5ade903cdb605980e27f89b400a614192d3e46fa
Belal Khan <probelalkhan@gmail.com>
2023-04-26 20:40:53 +0530

    Face Mesh Detection

---
+++ app/src/main/java/net/simplifiedcoding/mlkitsample/facemeshdetector/FaceMeshDetectionActivity.kt
@@ -0,0 +1,113 @@
+package net.simplifiedcoding.mlkitsample.facemeshdetector
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.facemesh.FaceMeshDetection
+import com.google.mlkit.vision.facemesh.FaceMeshDetector
+import com.google.mlkit.vision.facemesh.FaceMeshDetectorOptions
+import net.simplifiedcoding.mlkitsample.CameraXViewModel
+import net.simplifiedcoding.mlkitsample.R
+import net.simplifiedcoding.mlkitsample.databinding.ActivityFaceDetectionBinding
+import net.simplifiedcoding.mlkitsample.facedetector.FaceBox
+import net.simplifiedcoding.mlkitsample.facedetector.FaceDetectionActivity
+import java.util.concurrent.Executors
+
+class FaceMeshDetectionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFaceDetectionBinding
+    private lateinit var cameraSelector: CameraSelector
+    private lateinit var processCameraProvider: ProcessCameraProvider
+    private lateinit var cameraPreview: Preview
+    private lateinit var imageAnalysis: ImageAnalysis
+
+    private val cameraXViewModel = viewModels<CameraXViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityFaceDetectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_FRONT).build()
+        cameraXViewModel.value.processCameraProvider.observe(this) { provider ->
+            processCameraProvider = provider
+            bindCameraPreview()
+            bindInputAnalyser()
+        }
+    }
+
+    private fun bindCameraPreview() {
+        cameraPreview = Preview.Builder()
+            .setTargetRotation(binding.previewView.display.rotation)
+            .build()
+        cameraPreview.setSurfaceProvider(binding.previewView.surfaceProvider)
+        try {
+            processCameraProvider.bindToLifecycle(this, cameraSelector, cameraPreview)
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message ?: "IllegalArgumentException")
+        }
+    }
+
+    private fun bindInputAnalyser() {
+        val detector = FaceMeshDetection.getClient()
+
+        imageAnalysis = ImageAnalysis.Builder()
+            .setTargetRotation(binding.previewView.display.rotation)
+            .build()
+
+        val cameraExecutor = Executors.newSingleThreadExecutor()
+
+        imageAnalysis.setAnalyzer(cameraExecutor) { imageProxy ->
+            processImageProxy(detector, imageProxy)
+        }
+
+        try {
+            processCameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis)
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message ?: "IllegalArgumentException")
+        }
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun processImageProxy(detector: FaceMeshDetector, imageProxy: ImageProxy) {
+        val inputImage =
+            InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+        detector.process(inputImage).addOnSuccessListener { faces ->
+            faces.forEach { face ->
+                //@Todo draw face mesh here
+            }
+        }.addOnFailureListener {
+            it.printStackTrace()
+        }.addOnCompleteListener {
+            imageProxy.close()
+        }
+    }
+
+    companion object {
+        private val TAG = FaceMeshDetectionActivity::class.simpleName
+        fun startActivity(context: Context) {
+            Intent(context, FaceMeshDetectionActivity::class.java).also {
+                context.startActivity(it)
+            }
+        }
+    }
+}
